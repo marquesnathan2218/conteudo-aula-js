425fea4b7f50a97b157c5a5a298dd74b6a62dac6
Nathan Marques <marquesnathan2215@gmail.com>
2026-02-24 22:13:05 -0300

    Variaveis em JavaScript

---
+++ AULA1/src/script.js
@@ -0,0 +1,12 @@
+console.log
+
+//Exemplos de variavel let
+
+let nome = 'Nathan'
+
+console.log(nome);
+
+//Exemplos de variavel const
+const times = ['São Paulo', 'Corinthians', 'Santos'];
+
+console.log(times);
