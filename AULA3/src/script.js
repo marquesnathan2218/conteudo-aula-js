8bf1a7e0a2540ca7fd88cb88c2d703dfa1f1bac7
Nathan Marques <marquesnathan2215@gmail.com>
2026-02-26 22:20:14 -0300

    Lógica de programação em JavaScript

--- AULA3/src/script.js
+++ AULA3/src/script.js
@@ -1,2 +1,33 @@
-//  Exemplo de operação aritmética
-let a = 5;
+//Exemplo de Condição de lógica: if e else
+
+//Exemplo de Condição Lógica: If Else If
+let y = 2;
+
+if (y > 3) {
+    console.log('A variavel y é maior que 3!');
+}else if (y == 3){
+    //console.log(' a variavel y é igual a 3');
+}else{
+    //console.log('A variavel y possui um valor diferente das variações');
+}
+
+// Exemplo de if
+const elemento =  document.getElementById('ex1');
+const pegarHoras = new Date().getHours();
+const pegarMinutos = new Date().getMinutes();
+const hora= String(pegarHoras) + ":" + String(pegarMinutos);
+
+if (pegarHoras > 18) {
+    elemento.innerHTML = "Boa Noite!";
+}
+
+//Exemplo if 2
+let idade = 18;
+let texto = 'Voce é novo demais para dirigir';
+
+if (idade >= 18){
+    texto = 'Voce tem idade para dirigir';
+}
+
+document.getElementById('idade').innerHTML = 'Sua idade é: ' + idade;
+document.getElementById('ex2').innerHTML = texto;
