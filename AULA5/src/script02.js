6d2a173fcbd5e3aee8399f043a3b21cce5acb1fb
Nathan Marques <marquesnathan2215@gmail.com>
2026-03-02 22:13:20 -0300

    Aula 5 - Laço de Repetição

---
+++ AULA5/src/script02.js
@@ -0,0 +1,13 @@
+//Exemplo de operador NOT
+
+let usuarioLogado = false;
+
+if (usuarioLogado){
+    console.log('teste');
+}
+
+let lista = [];
+
+if (!lista.length){
+    console.log('A lista está vazia');
+}
